5b9cf73344b611bc5011b8046e9b72ea47011dfd
bwolfson2 <bwolfson@alumni.princeton.edu>
2025-12-16 01:04:57 +0200

    added crons

--- test.php
+++ test.php
@@ -1,3 +1,4 @@
+tee test.php <<'EOF'
 <?php
 return array(
     'db_driver' => 'mysqli',
@@ -8,8 +9,8 @@ return array(
 'contact_list' => array(' m_facility_key'  =>  'm_facility_key',' m_full_address'  =>  'm_full_address',' m_gender'  =>  'm_gender',' m_grd_yr'  =>  'm_grd_yr',' m_group_label'  =>  'm_group_label',' m_match_score'  =>  'm_match_score',' m_match_type'  =>  'm_match_type',' m_matched_facility'  =>  'm_matched_facility',' m_med_sch'  =>  'm_med_sch',' m_npi'  =>  'm_npi',' m_num_org_mem'  =>  'm_num_org_mem',' m_org_dr_count'  =>  'm_org_dr_count',' m_org_linked_npi_count'  =>  'm_org_linked_npi_count',' m_org_other_med_count'  =>  'm_org_other_med_count',' m_org_pa_np_count'  =>  'm_org_pa_np_count',' m_org_pac_id'  =>  'm_org_pac_id',' m_org_phys_counts'  =>  'm_org_phys_counts',' m_org_site_count'  =>  'm_org_site_count',' m_org_site_phys_max'  =>  'm_org_site_phys_max',' m_org_site_phys_total'  =>  'm_org_site_phys_total',' m_org_state_count'  =>  'm_org_state_count',' m_pri_spec'  =>  'm_pri_spec',' m_primary_spec'  =>  'm_primary_spec',' m_provider_middle_name'  =>  'm_provider_middle_name',' m_sec_spec_all'  =>  'm_sec_spec_all',' m_site_facility_name'  =>  'm_site_facility_name',' m_site_key'  =>  'm_site_key',' m_site_phys_counts'  =>  'm_site_phys_counts'),
 'company_list' => array(' m_company_num_org_mem'  =>  'm_company_num_org_mem',' m_company_org_dr_count'  =>  'm_company_org_dr_count',' m_company_org_other_med_count'  =>  'm_company_org_other_med_c',' m_company_org_pa_np_count'  =>  'm_company_org_pa_np_count',' m_company_org_pac_id'  =>  'm_company_org_pac_id',' m_company_org_phys_counts'  =>  'm_company_org_phys_counts',' m_company_org_site_count'  =>  'm_company_org_site_count',' m_company_org_site_phys_max'  =>  'm_company_org_site_phys_m',' m_company_org_site_phys_total'  =>  'm_company_org_site_phys_t',' m_company_org_state_count'  =>  'm_company_org_state_count'),
 )
-
 );
+EOF
 
 
 
